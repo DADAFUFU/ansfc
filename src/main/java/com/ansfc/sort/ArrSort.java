71b12e71ec6454d09f838d03cae4a885a1eb5bb7
sunfc <sunfucheng@xiaomi.com>
2020-02-04 12:33:59 +0800

    排序算法

--- src/main/java/com/ansfc/sort/ArrSort.java
+++ src/main/java/com/ansfc/sort/ArrSort.java
@@ -1,7 +1,16 @@
 package com.ansfc.sort;
 
+import java.util.Arrays;
+
 public class ArrSort {
 
+
+    public static void main(String[] args){
+        int[] arr = {2,4,3,1,5,6,9,8,7};
+        ArrSort sort = new ArrSort();
+        sort.quickSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
 
     int mpartition(int a[], int l, int r) {
         int pivot = a[l];
@@ -85,6 +94,95 @@ public class ArrSort {
                 a[j+1]=a[j];
             a[j+1]=v;
         }
+    }
+
+
+    void selectSort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i]>arr[j]){
+                    swap(arr,i,j);
+                }
+            }
+        }
+    }
+
+    void bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+
+    void insertSort(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j] > arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 快速排序
+     * @param arr
+     * @param left
+     * @param right
+     */
+    void quickSort(int[] arr,int left,int right){
+        if(left < right){
+            int middle = middle(arr,left,right);
+            quickSort(arr,left,middle-1);
+            quickSort(arr,middle+1,right);
+        }
+
+
+    }
+
+    /**
+     * 算出每次快速排序的中间位
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private int middle(int[] arr, int left, int right) {
+        int index = arr[left];
+        while(left < right){
+
+            while(left<right && arr[right]>index){
+                right--;
+            }
+            if(left<right){
+                arr[left] = arr[right];
+                left++;
+            }
+            while(left< right && arr[left]< index){
+                left++;
+            }
+            if(left<right){
+                arr[right] = arr[left];
+                right--;
+            }
+        }
+        arr[left] = index;
+        return left;
+    }
+
+
+    void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
 
